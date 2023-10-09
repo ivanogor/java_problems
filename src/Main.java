3e15bf5589fcf6fb7353709250315e2a2cd071db
ivanogor <ivanogor23@gmail.com>
2023-10-09 21:55:49 +0800

    leetcode problems number 412.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int max = 0;
-        for(int i[] : accounts){
-            int sum = 0;
-            for(int j : i){
-                sum += j;
-            }
-            max = Math.max(sum, max);
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>(n);
+        for(int i = 1; i <= n; i++){
+            boolean true3 = i % 3 == 0;
+            boolean true5 = i % 5 == 0;
+            if(true3 && true5){
+                answer.add("FizzBuzz");
+            } else if(true3){
+                answer.add("Fizz");
+            } else if(true5){
+                answer.add("Buzz");
+            } else answer.add(String.valueOf(i));
         }
-        return max;
+        return answer;
     }
 }
