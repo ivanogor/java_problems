a66bd47eacb66e3b28d1565e807588ad19bd55ae
ivanogor <ivanogor23@gmail.com>
2023-10-09 22:01:25 +0800

    leetcode problems number 1342.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 class Solution {
-    public List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<>(n);
-        for(int i = 1; i <= n; i++){
-            boolean true3 = i % 3 == 0;
-            boolean true5 = i % 5 == 0;
-            if(true3 && true5){
-                answer.add("FizzBuzz");
-            } else if(true3){
-                answer.add("Fizz");
-            } else if(true5){
-                answer.add("Buzz");
-            } else answer.add(String.valueOf(i));
+    public int numberOfSteps(int num) {
+        int c = 0;
+        while(num != 0){
+            if(num % 2 == 0){
+                num /= 2;
+                c++;
+            }else {
+                num -= 1;
+                c++;
+            }
         }
-        return answer;
+        return c;
     }
 }
