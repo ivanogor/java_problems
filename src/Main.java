a76a34bd87c544403d4c95654fde7bc5197a5eee
ivanogor <ivanogor23@gmail.com>
2023-10-09 22:12:04 +0800

    leetcode problems number 876.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 class Solution {
-    public int numberOfSteps(int num) {
-        int c = 0;
-        while(num != 0){
-            if(num % 2 == 0){
-                num /= 2;
-                c++;
-            }else {
-                num -= 1;
-                c++;
-            }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return c;
+        return slow;
     }
 }
