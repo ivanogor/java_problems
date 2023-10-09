ff65495c458cd1ce3a46afa57f4afb1212333249
ivanogor <ivanogor23@gmail.com>
2023-10-09 21:25:41 +0800

    leetcode problems number 1480.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
     }
 }
