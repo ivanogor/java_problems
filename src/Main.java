0ed275c19b88e2395f2908ce8e029798aef97180
ivanogor <ivanogor23@gmail.com>
2023-10-12 09:44:37 +0800

    leetcode problems number 169.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int k = 1;
-        for(int i = 1; i<nums.length; i++){
-            if(nums[i] != nums[i-1]){
-                nums[k] = nums[i];
-                k++;
-            }
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int b = nums.length / 2;
+        int d = 1;
+        int ans = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i+1]){
+                d++;
+                if(d > b){
+                    ans = nums[i];
+                    break;
+                }
+            }else d = 1;
         }
-        return k;
+        if(nums.length == 1) ans = nums[0];
+        return ans;
     }
 }
